089117643abff37c850298e25b58cede18af17d5
zhaoshiji123 <47933549+zhaoshiji123@users.noreply.github.com>
2023-03-29 21:15:19 +0800

    Update resnet18_mtard_cifar10.py

--- resnet18_mtard_cifar10.py
+++ resnet18_mtard_cifar10.py
@@ -52,22 +52,6 @@ init_loss_nat = None
 init_loss_adv = None
 
 
-class AverageMeter(object):
-    """Computes and stores the average and current value"""
-    def __init__(self):
-        self.reset()
-
-    def reset(self):
-        self.val = 0
-        self.avg = 0
-        self.sum = 0
-        self.count = 0
-
-    def update(self, val, n=1):
-        self.val = val
-        self.sum += val * n
-        self.count += n
-        self.avg = self.sum / self.count
 
 def kl_loss(a,b):
     loss = -a*b + torch.log(b+1e-5)*b
